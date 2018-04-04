874165006338400e7b6d9a71a85c8cbbec89c826
guaracyalima <guaracyaraujolima@gmail.com>
2018-04-04 14:03:03 -0300

    full broker

--- src/app/bseg/broker/broker-list/broker-list.component.ts
+++ src/app/bseg/broker/broker-list/broker-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BrokerService} from "../../../services/broker.service";
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-broker-list',
@@ -9,15 +10,36 @@ import {BrokerService} from "../../../services/broker.service";
 export class BrokerListComponent implements OnInit {
 
   public broker: any;
-  constructor(private _db: BrokerService) { }
+
+  constructor(private _db: BrokerService) {
+  }
 
   ngOnInit() {
-      this.getAll()
+    this.getAll()
   }
 
 
   public getAll(): void {
-    this._db.index().subscribe( data => this.broker = data, err => console.log('erro no get da parada', err))
+    this._db.index().subscribe(data => this.broker = data, err => console.log('erro no get da parada', err))
+  }
+
+  public destroy(id: number) {
+    this._db.destroy(id).subscribe(data => {
+        this.getAll()
+        swal('Sucesso', 'Corretora excluida com sucesso', 'success')
+      },
+      error => console.error('Erro ao excluir corretora'));
+  }
+
+
+  public removeDepartament(id: number) {
+    this._db.destroyDepartament(id)
+      .subscribe(success => {
+        this.getAll()
+        swal('Sucesso', 'Departamento excluido com sucesso!', 'success')
+      }, error => {
+        swal('Error', 'Erro ao excluir o departamento!', 'error')
+      })
   }
 
 }
