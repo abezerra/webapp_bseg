7c03b45650027c7c3406e91b6932f4260663a1c7
guaracyalima <guaracyaraujolima@gmail.com>
2018-04-11 11:14:38 -0300

    add apiUri of production

--- src/environments/api.ts
+++ src/environments/api.ts
@@ -1,5 +1,6 @@
 const api = {
-    apiUrl:  'http:///127.0.0.1:8000/api'
+    apiUrl:  'https://api-seguradora-staging.herokuapp.com/api'
 };
 
+// apiUrl:  'http:///127.0.0.1:8000/api'
 export default api
